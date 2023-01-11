5fccff67babc117f918abb46836d393fa714577a
Adrian Knuutinen <adrianknuutinen@gmail.com>
2023-01-11 10:12:17 +0100

    second commit

--- linkedlist.py
+++ linkedlist.py
@@ -1,19 +1,19 @@
 import os
 import re
 
-
+# clear function clears the console
 def clear(): return os.system('cls')
 
 
 clear()
 
-
+# Node class for creating individual nodes in the linked list
 class Node:
     def __init__(self, product, next_node=None):
         self.product = product
         self.next_node = next_node
 
-
+# LinkedList class for creating the linked list and adding products
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -30,17 +30,23 @@ class LinkedList:
             current_node = current_node.next_node
 
 
+# create an instance of the LinkedList class
 product_list = LinkedList()
 
+# infinite loop for getting user input and adding products to the list
 while True:
     user_input = input("Enter a two-letter code followed by a dash and a number between 200 and 700 (XX-###) or type 'exit' to exit: ")
+    # exit the loop if user inputs "exit"
     if user_input.strip().lower() == "exit":
         break
+    # check if input is empty
     if not user_input:
         print("Input is empty. Please try again.")
         continue
+    # check if input matches the pattern
     pattern = r"^[a-zA-Z]{2}-\d{3}$"
     if re.match(pattern, user_input):
+        # check if number is between 200 and 700
         number = int(user_input[3:])
         if 200 <= number <= 700:
             product_list.add_product(user_input)
@@ -51,4 +57,5 @@ while True:
     else:
         print("Input does not match pattern. Please try again.")
         continue
+# print products in the list
 product_list.print_products()
